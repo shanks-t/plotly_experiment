6e6019756cbee50af3616d834ebc2e4e08a37333
shanks-t <shanksleslietrey@gmail.com>
2023-09-30 09:11:30 -0500

    increase memory for classmates to use

--- infra/main.tf
+++ infra/main.tf
@@ -236,10 +236,10 @@ resource "aws_ecs_task_definition" "this" {
       }
     }
   }])
-  cpu                      = 256
+  cpu                      = 2048
   execution_role_arn       = aws_iam_role.this.arn
   family                   = "family-of-${local.example}-tasks"
-  memory                   = 512
+  memory                   = 4096
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
   runtime_platform {
